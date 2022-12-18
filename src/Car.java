aef3e351f19159850a79c78c6a61046a514e3ab6
helloworld <sergeychernonosov21229@gmail.com>
2022-12-18 20:11:04 +0300

    Сделал задание 3

--- src/Car.java
+++ src/Car.java
@@ -13,13 +13,43 @@ public class Car {
                 color + " цвет кузова, обьем двигателя " + engineVolume + " - л.";
     }
 
-    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+
+        if (brand == null || brand == ""){
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+
+        if (model == null || model == ""){
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+
+        if (country == null || country == ""){
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        if (color == null || color == "") {
+            this.color = "Белый";
+        } else {
+            this.color = color;
+        }
+
+        if (year < 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
 
     }
 
